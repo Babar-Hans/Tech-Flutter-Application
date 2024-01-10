72f4682181b7669755acfe1604833a090f06b762
Babar Hans <babarhans34@gmail.com>
2024-01-10 19:39:21 +0500

    for loop start with table practice

--- main.dart
+++ main.dart
@@ -1,15 +1,17 @@
 void main(){
  
-  List<String> section = ["Babar","Ali","Sahil"];
-
-   // two dimension lists
-  List<List<String>> section1 = [
-    //row 0
-    section,
-    //row 1
-    ['Jamal','Shahid','Ahmed'],
-    //row2
-    ['Amir','Faraz','John']
-  ];
-  print(section1[2][2]);
+  // List<String> section = ["Babar","Ali","Sahil"];
+  //  // two dimension lists
+  // List<List<String>> section1 = [
+  //   //row 0
+  //   section,
+  //   //row 1
+  //   ['Jamal','Shahid','Ahmed'],
+  //   //row2
+  //   ['Amir','Faraz','John']
+  // ];
+  // print(section1[2][2]);
+  for(int i = 1; i<=10; i++){
+    print("2 x $i = ${i*2}");
+  }
 }
