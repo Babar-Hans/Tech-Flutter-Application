16906b23ea0b3410dbc4fc82dd968686022b5480
Babar Hans <babarhans34@gmail.com>
2024-01-13 19:17:45 +0500

    working

--- functions.dart
+++ functions.dart
@@ -1,5 +1,6 @@
 void main(){
-
-
-  
+  int numOfTeaCups = 2;
+  String fromSource = "canteen";
+  int sugarAmount = 2;
+  print("chai lai aya $numOfTeaCups cups $fromSource sai $sugarAmount spoon cheeni dali hai");
 }
